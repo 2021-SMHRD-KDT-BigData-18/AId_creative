496b27db7078fad4b93f8e84c35a97801a11b8c5
smhrd <smhrd@DESKTOP-55J15SH>
2023-07-18 17:24:24 +0900

    비밀번호 암호화

---
+++ AId_creative/src/main/java/kr/AId_creative/controller/UserSha256.java
@@ -0,0 +1,31 @@
+package kr.AId_creative.controller;
+
+import java.security.MessageDigest;
+
+public class UserSha256 {
+	public static String encrypt(String planText) {
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(planText.getBytes());
+			byte byteData[] = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			StringBuffer hexString = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				String hex = Integer.toHexString(0xff & byteData[i]);
+				if (hex.length() == 1) {
+					hexString.append('0');
+				}
+				hexString.append(hex);
+			}
+			// hexString.substring(0,30);
+
+			return hexString.substring(0, 30);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException();
+		}
+	}
+}
